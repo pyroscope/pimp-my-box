d00017f3b965214dfb316999d3606402f57dc197
pyroscope <pyroscope.project@gmail.com>
2017-03-08 00:32:37 +0100

    added 'rtls' shell alias

--- roles/pyroscope-cli/files/profile.d/rt-alias.sh
+++ roles/pyroscope-cli/files/profile.d/rt-alias.sh
@@ -13,6 +13,7 @@ alias rtjustnow="rtcontrol loaded=-5i -qofiles"
 alias rt2days="rt-completion completed=-2d"
 alias rt7days="rt-completion completed=-7d"
 alias rt-completion="rtcontrol --column-headers -scompleted -ocompletion"
+alias rtls="rtcontrol -qo '{{chr(10).join([d.directory+chr(47)+x.path for x in d.files])}}'"
 alias rt-stats-msg="rtcontrol -q -s alias,is_open,message -o alias,is_open,message 'message=?*' message=\!*Tried?all?trackers* | uniq -c"
 alias rt-stats-seeding='rtcontrol --summary -qco leechtime,seedtime,size.sz,uploaded.sz,ratio.pc'
 alias rt-stats-trackers='rtcontrol -s alias -qo alias '\''*'\'' | uniq -c | sort -n'
