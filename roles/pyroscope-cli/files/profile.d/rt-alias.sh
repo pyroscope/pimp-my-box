5528b2dc9cf632f842d036906de0df74bceebcff
pyroscope <pyroscope.project@gmail.com>
2017-03-08 07:55:14 +0100

    added rt-ui-* aliases

--- roles/pyroscope-cli/files/profile.d/rt-alias.sh
+++ roles/pyroscope-cli/files/profile.d/rt-alias.sh
@@ -4,10 +4,10 @@
 #
 # "pimp-my-box" default shell aliases
 
-# activate 'pyrocore' virtualenv
+# Activate 'pyrocore' virtualenv
 test ! -f ~/lib/pyroscope/bin/activate || . ~/lib/pyroscope/bin/activate
 
-# rtorrent aliases
+# rTorrent aliases
 alias rtlistmethods='rtxmlrpc system.listMethods | egrep'
 alias rtjustnow="rtcontrol loaded=-5i -qofiles"
 alias rt2days="rt-completion completed=-2d"
@@ -17,5 +17,9 @@ alias rtls="rtcontrol -qo '{{chr(10).join([d.directory+chr(47)+x.path for x in d
 alias rt-stats-msg="rtcontrol -q -s alias,is_open,message -o alias,is_open,message 'message=?*' message=\!*Tried?all?trackers* | uniq -c"
 alias rt-stats-seeding='rtcontrol --summary -qco leechtime,seedtime,size.sz,uploaded.sz,ratio.pc'
 alias rt-stats-trackers='rtcontrol -s alias -qo alias '\''*'\'' | uniq -c | sort -n'
+
+# Shortcuts to internal commands
+alias rt-ui-categories="python-pyrocore -m pyrocore.ui.categories"
+alias rt-ui-themes="python-pyrocore -m pyrocore.ui.theming"
 
 # END rt-alias.sh
